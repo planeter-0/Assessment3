4b8f7d443bde4f2f9c1e5146b352ad615a27642a
planeter-0 <849407329@qq.com>
2020-12-19 01:25:19 +0800

    null

---
+++ src/main/java/ManagementSystem.java
@@ -0,0 +1,9 @@
+public class ManagementSystem {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
